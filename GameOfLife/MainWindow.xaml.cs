b74cba01d628212518fc0124f73eb474796133d1
david.dasilva1 <david.dasilva@he-arc.ch>
2019-12-04 09:12:40 +0100

    add begining project

---
+++ GameOfLife/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameOfLife
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            for (int i=0;i<20;i++)
+            {
+                for(int j=0;j<20;j++)
+                {
+
+
+                    System.Windows.Shapes.Rectangle rectangle = new System.Windows.Shapes.Rectangle();
+
+                    rectangle.Name = "rectCol" + i.ToString() + "Row" + j.ToString();
+                    rectangle.Fill = new SolidColorBrush(System.Windows.Media.Colors.Turquoise);
+                    rectangle.Stroke = new SolidColorBrush(System.Windows.Media.Colors.Black);
+
+                    Grid.SetColumn(rectangle, i);
+                    Grid.SetRow(rectangle, j);
+
+                    myGrid.Children.Add(rectangle);
+                }
+
+            }
+
+
+
+
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            string selectedRect;
+            if (e.OriginalSource is Rectangle)
+            {
+                Rectangle ClickedRectangle = (Rectangle)e.OriginalSource;
+
+                var mouseWasDownOn = e.Source as FrameworkElement;
+                string elementName = mouseWasDownOn.Name;
+                selectedRect = elementName.ToString();
+                MessageBox.Show(selectedRect.ToString());
+                
+            }
+        }
+
+    }
+}
