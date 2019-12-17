c5f8cb075369031d15828c2cc19d7bfa53e4104f
Jeannet Nino <nino.jeannet@he-arc.ch>
2019-12-17 11:26:13 +0100

    add second template

--- GameOfLife/MainWindow.xaml.cs
+++ GameOfLife/MainWindow.xaml.cs
@@ -24,6 +24,8 @@ namespace GameOfLife
         private bool isDown = false;
         private Cell[,] cells;
         private int nbOfRowCell = 20;
+        private int nbOfColumnCell;
+
 
         private HashSet<Tuple<int, int>> tupleList = new HashSet<Tuple<int, int>>();
 
@@ -51,17 +53,17 @@ namespace GameOfLife
                 myGrid.RowDefinitions.Add(gridRow);
 
             }
-            int nbOfColumn = Convert.ToInt32(SystemParameters.WorkArea.Width / cellSize);
-            for (int i = 0; i < nbOfColumn; i++)
+            nbOfColumnCell = Convert.ToInt32(SystemParameters.WorkArea.Width / cellSize);
+            for (int i = 0; i < nbOfColumnCell; i++)
             {
                 ColumnDefinition gridCol = new ColumnDefinition();
                 gridCol.Width = new GridLength(cellSize);
 
                 myGrid.ColumnDefinitions.Add(gridCol);
             }
-            cells = new Cell[nbOfColumn, nbOfRowCell];
+            cells = new Cell[nbOfColumnCell, nbOfRowCell];
 
-            for (int i = 0; i < nbOfColumn; i++)
+            for (int i = 0; i < nbOfColumnCell; i++)
             {
                 for (int j = 0; j < nbOfRowCell; j++)
                 {
@@ -158,15 +160,40 @@ namespace GameOfLife
 
 
         /// eventuellement faire une seule méthode pour les 3 template en passant le numéro du template en args
-        private void setTemplate1(object sender, RoutedEventArgs e)
+        private void setTemplate(object sender, RoutedEventArgs e)
         {
-            int x=0, y = 0; // Top left position of the template
+             // Top left position of the template
             List<Tuple<int,int>> positionList = new List<Tuple<int, int>>();
-            positionList.Add(new Tuple<int, int>(x, y+2));
-            positionList.Add(new Tuple<int, int>(x+1, y+2));
-            positionList.Add(new Tuple<int, int>(x+2, y+2));
-            positionList.Add(new Tuple<int, int>(x+1, y));
-            positionList.Add(new Tuple<int, int>(x+2, y+1));
+            Button btn = (Button)sender;
+            if(btn.Name == "template1")
+            {
+                int x = 0, y = 0;
+                positionList.Add(new Tuple<int, int>(x, y + 2));
+                positionList.Add(new Tuple<int, int>(x + 1, y + 2));
+                positionList.Add(new Tuple<int, int>(x + 2, y + 2));
+                positionList.Add(new Tuple<int, int>(x + 1, y));
+                positionList.Add(new Tuple<int, int>(x + 2, y + 1));
+            }
+            else if(btn.Name == "template2")
+            {
+                int x = nbOfColumnCell/2-5, y = nbOfRowCell/2;
+                positionList.Add(new Tuple<int, int>(x, y));
+                positionList.Add(new Tuple<int, int>(x + 1,y));
+                positionList.Add(new Tuple<int, int>(x + 2, y));
+                positionList.Add(new Tuple<int, int>(x + 3, y));
+                positionList.Add(new Tuple<int, int>(x + 4, y));
+                positionList.Add(new Tuple<int, int>(x + 5, y));
+                positionList.Add(new Tuple<int, int>(x + 6, y));
+                positionList.Add(new Tuple<int, int>(x + 7, y));
+                positionList.Add(new Tuple<int, int>(x + 8, y));
+                positionList.Add(new Tuple<int, int>(x + 9, y));
+
+            }
+            else
+            {
+
+            }
+
             resetCells();
             foreach(Tuple<int, int> cell in positionList)
             {
